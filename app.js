00483d2ee3917d60e9f83d659d0e73bb9f24d81e
cherrystz <plug4869@gmail.com>
2021-10-01 20:17:30 +0700

    v0.22 add .gitignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-let port = process.env.PORT;
+let port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
